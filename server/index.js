a6e1cf42d5ceaf1fa835ab26fa1dd10f8982d1fe
unknown <duyka6203@gmail.com>
2024-06-12 14:44:48 +0700

    update index.js

--- server/index.js
+++ server/index.js
@@ -25,7 +25,13 @@ app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("dev"));
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: "https://connectu-lemon.vercel.app", // Replace with your frontend URL
+    credentials: true, // This is needed if you're sending cookies with requests
+  })
+);
 // app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 // /* ROUTES */
